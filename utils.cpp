e421b7756587acc6b4307dfd3d3977a06c258323
esuazo2601 <esteban.suazo.m2601@gmail.com>
2023-11-30 23:06:24 -0300

    Init Repo

---
+++ utils.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include "utils.h"
+using namespace std;
+
+int hamming_cuadrado_a(std::string sol_in, vector<std::string>entrada){
+    int len = sol_in.length();
+    int len_entrada = entrada.size();
+    vector<int>costos;
+
+    for (int i=0;i<len_entrada;i++){
+        int costo = 0;
+        for (int j=0 ; j<len ; j++){
+            if(sol_in[j] != entrada[i][j]){
+                costo++;
+            }
+        }
+        costos.push_back(costo);
+    }
+
+    int costos_len = costos.size();
+    int costo_total=0;
+
+    for (int i=0 ;i<costos_len ;i++){
+        costo_total += costos[i]*costos[i];
+    }
+    return costo_total;
+}
+
+char pick_random_letter(){
+    string DNA = "ATCG";
+    return DNA [rand()%DNA.size()];
+}
+
+vector<string> lee_instancia(string nombre){
+    ifstream archivo_txt(nombre);
+    vector<string> mat;
+    string linea;
+    while (getline(archivo_txt, linea)) {
+        mat.push_back(linea);
+    }
+    archivo_txt.close();
+    return mat;
+}
---
+++ utils.h
@@ -0,0 +1,6 @@
+#include <vector>
+#include <string>
+
+int hamming_cuadrado_a(std::string sol_in, std::vector<std::string>entrada);
+char pick_random_letter();
+std::vector<std::string> lee_instancia(std::string nombre);
